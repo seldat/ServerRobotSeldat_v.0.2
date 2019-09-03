c8ddfe5a8ebf02fea2756cdca40bf0f60a5a65c0
ChauNguyen <ngocchau.ann@gmail.com>
2019-09-03 12:43:22 +0700

    Change time out keepalive rb to 100ms

--- SeldatUnilever_Ver1.02/Management/RobotManagent/RobotUnityControl.cs
+++ SeldatUnilever_Ver1.02/Management/RobotManagent/RobotUnityControl.cs
@@ -258,7 +258,7 @@ namespace SeldatMRMS.Management.RobotManagent
         public RobotUnityControl()
         {
             timerCheckKeepAlive = new Timer();
-            timerCheckKeepAlive.Interval = 250;
+            timerCheckKeepAlive.Interval = 100;
             timerCheckKeepAlive.Elapsed += checkKeepAliveEvent;
             timerCheckKeepAlive.AutoReset = true;
             timerCheckKeepAlive.Enabled = true;
