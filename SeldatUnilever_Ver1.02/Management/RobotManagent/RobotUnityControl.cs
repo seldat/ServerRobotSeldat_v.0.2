6ae1287eef01ff73cffc2c0834332fb77d4020ad
luattran <tranhuuluat@gmail.com>
2019-10-15 07:42:00 +0700

    cap nhat checkalive 500ms and ConfigZone

--- SeldatUnilever_Ver1.02/Management/RobotManagent/RobotUnityControl.cs
+++ SeldatUnilever_Ver1.02/Management/RobotManagent/RobotUnityControl.cs
@@ -269,7 +269,7 @@ namespace SeldatMRMS.Management.RobotManagent
         public RobotUnityControl()
         {
             timerCheckKeepAlive = new Timer();
-            timerCheckKeepAlive.Interval = 100;
+            timerCheckKeepAlive.Interval = 500;
             timerCheckKeepAlive.Elapsed += checkKeepAliveEvent;
             timerCheckKeepAlive.AutoReset = true;
             timerCheckKeepAlive.Enabled = true;
